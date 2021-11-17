0df22dac586f359f338d71e67017e1a77d0ce550
Thomas Luijkman <thomasluijkman@gmail.com>
2021-11-17 15:03:50 +0100

    summary character limit added

--- packet.py
+++ packet.py
@@ -3,8 +3,22 @@ import pyshark
 class Packet:
     def __init__(self, data, summary, analysis):
         self.data = data
-        self.summary = summary
+        self.summary = self.process_summary(summary)
         self.eval = analysis
+
+    def process_summary(self, summary):
+        words = summary.split(' ')[2:]
+        sentence = []
+        for word in words:
+            if not word:
+                continue
+            if (len(' '.join(sentence)) + len(word)) < 60:
+                sentence.append(word)
+            else:
+                sentence.append('...')
+                break
+        sentence = ' '.join(sentence)
+        return sentence
 
     def get_data(self):
         return self.data
